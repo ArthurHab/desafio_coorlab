7f201b862b5c72479b9d7350cdde3d5bb55fb17b
ArthurHab <arthur200106@gmail.com>
2023-06-12 11:00:06 -0300

    create BaseButton

---
+++ code/src/components/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <button class="button" @click="action">{{text}}</button>
+</template>
+
+<script>
+export default{
+    props: ['action','text'],
+}
+</script>
+
+<style>
+.button{
+    padding: 5px 30px;
+    background-color: cadetblue;
+    font-size: 14px;
+    border: none;
+    border-radius: 10px;
+}
+</style>
